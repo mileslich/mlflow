d5dc3acffb51b95f32320f3a5ca6dc110db94f99
mileslich <miles.lich@outlook.com>
2022-02-19 11:43:21 -0500

    change

--- docs/build-rdoc.sh
+++ docs/build-rdoc.sh
@@ -8,7 +8,7 @@ image_name="mlflow-r-dev"
 docker build -f Dockerfile.dev -t $image_name .
 docker run \
   --rm \
-  -v $(pwd):\\mlflow\\mlflow\\R\\mlflow \
+  -v \\$(pwd):\\mlflow\\mlflow\\R\\mlflow \
   -v /$(pwd)//../../../docs/source:/mlflow/docs/source \
   $image_name \
   Rscript -e 'source(".build-doc.R", echo = TRUE)'
